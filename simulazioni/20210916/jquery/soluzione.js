ec3c9d4647d9c20b4f0c0ea724c9a135fa316ddd
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-02-15 19:50:34 +0100

    Update soluzione.js

---
+++ simulazioni/20210916/jquery/soluzione.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    /*
+    let table = document.createElement('table');
+    let tbody = document.createElement('tbody');
+    let row = document.createElement('tr');
+    for(i = 1; i <10; i++) {
+        let row_data = document.createElement('td');
+        row_data.innerHTML = i;
+        row.appendChild(row_data);
+    }
+    tbody.appendChild(row);
+    table.appendChild(tbody);
+    $("p:last-child").append("<p>ciao</p>");
+    
+    const h1 = document.createElement("H1");
+    const textNode = document.createTextNode("Hello World");
+    h1.appendChild(textNode);
+    $(document).appendChild(h1);
+    */
+   alert("funziona");
+ });
